d7298bed8ded76b3822549d45c77b6039a7d4fb2
Steve Klabnik <steve@steveklabnik.com>
2012-01-12 13:57:54 -0500

    introducing tests for the new class.
    I'm leaving the old tests in for now, to show that we're passing both.
    Since the tests are so similar, we can show that we have a high
    level of confidence that we're testing the same behavior, and
    get rid of the old tests.

--- spec/geolocator_spec.rb
+++ spec/geolocator_spec.rb
@@ -42,3 +42,25 @@ describe Geolocator do
   end      
 end
 
+describe Geolocator::FreeGeoIp do  
+  describe "#geolocate_ip" do
+    it "successfully makes a HTTP request" do
+      expect{Geolocator::FreeGeoIp.new("231.4.8.6").geolocate_ip.success?}.to be_true
+    end
+    
+    it "throws an error for HTTP statuses other than 200" do
+      expect {Geolocator::FreeGeoIp.new("0.0.0.0").geolocate_ip}.to raise_error(RuntimeError,"IP address not found")
+    end
+    
+    it "parses the HTTP body with JSON" do
+      Geolocator::FreeGeoIp.new("123.45.6.28").geolocate_ip.should be_an_instance_of Hash
+    end
+    
+    it "has a value for city" do
+      @result = Geolocator::FreeGeoIp.new("123.45.6.28").geolocate_ip["city"].should_not be_empty
+      expect {Geolocator::FreeGeoIp.new("240.0.0.0").geolocate_ip}.to raise_error(RuntimeError,"Incomplete record. Please try another address")
+    end
+  end      
+end
+
+
